b48317b28333d7473147ab129bab660c14590861
syab-syab <syabsyabsyabsyab@gmail.com>
2023-09-18 21:03:55 +0900

    メモ追加

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import testData from './data/test-data.json'
 
 function App() {
 
+  // 格納されるJSONの型
   type Task = {
     id: number
     content: string
@@ -22,20 +23,20 @@ function App() {
     check: boolean
   }
 
+  // 必要ないかもしれんけど一応型定義
   const testTask: Array<Task> = testData['test-data']
-
-
   localStorage.setItem("test-task", JSON.stringify(testTask))
-  const getTask: any  = localStorage.getItem("test-task")
 
+  // string型だとエラーになるからやむを得ずanyにした
+  const getTask: any  = localStorage.getItem("test-task")
   console.log(getTask, typeof(getTask))
 
+  // なぜこの型定義で成功したのかいまいちわかってないので後日チェック
   const modiTask: Array<Task> = JSON.parse(getTask)
-
   console.log(modiTask)
 
+  // 個別に取り出すことが出来た
   const test = modiTask.filter(e => e.id === 1)
-
   console.log(test[0].content)
 
 
