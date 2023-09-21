11ee498c131c81cb4d061f7597827e40d4e31f1b
syab-syab <syabsyabsyabsyab@gmail.com>
2023-09-21 16:25:34 +0900

    関数修正

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -23,13 +23,13 @@ const Example = () => {
   const [inputValue, setInputValue] = useState<string>("");
 
   // 削除ボタンでtasks削除
-  const handleDelete = (id: number) => {
+  const handleDelete = (id: number): void => {
     const newTasks = tasks.filter((t) => t.id !== id)
     setTasks(newTasks)
   }
 
   // 新しいTaskの登録
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
 
     // 新しいTaskを作成
@@ -43,16 +43,17 @@ const Example = () => {
     // スプレッド構文
     setTasks([newTask, ...tasks])
     setInputValue("")
+    console.log(tasks)
   }
 
   // フォームの変更を検知
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     // console.log(e.target.value);
     setInputValue(e.target.value);
   }
 
   // checkの値を編集
-  const handleCheck = (id: number, check: boolean) => {
+  const handleCheck = (id: number, check: boolean): void => {
     const newTasks = tasks.map((t) => {
       if (t.id === id) {
         t.check = !check
@@ -64,13 +65,30 @@ const Example = () => {
   }
 
   // 期日を表示
-  const dateAp = (unix: string) => {
+  const dateAp = (unix: string): string => {
     if (!unix) {
       return "無し"
     }
     const tmp = Number(unix)
     const date = new Date(tmp)
     return `${date.getFullYear()}年 ${date.getMonth()}月 ${date.getDate()}日`
+  }
+
+
+
+  // 期日を過ぎているかどうか
+  const checkDueDate = (val: string): boolean => {
+    const tmp = Number(val)
+    const currentDateTime = new Date
+    // 期日を過ぎていない or 設定されていないなら true を返す
+    if (tmp > currentDateTime.getTime() || val === "") {
+      return true
+    // 過ぎているなら false を返す
+    } else if (currentDateTime.getTime() >= tmp ) {
+      return false
+    } else {
+      return true
+    }
   }
 
   return (
@@ -99,12 +117,16 @@ const Example = () => {
           />
         </form>
       </div>
+      <div style={{textAlign: "center", margin: "auto"}}>
       {
           tasks.map(task => {
           // return 付けないとエラー発生するから注意
           return (
-          <p key={task.id} style={{background: task.dueDate ? "green" : "white"}}>
+          // 期日設定の有無でスタイル変更
+          <p key={task.id} style={{borderBottom: task.dueDate ? "1rem solid green" : "", width: "auto", background: checkDueDate(task.dueDate) ? "rgba(255, 255, 128, .5)" : "gray"}}>
+            {/* チェックボックスのチェックの有無でデータのプロパティ変更 */}
             <input type="checkbox" onChange={() => handleCheck(task.id, task.check)} />
+            {/* checkプロパティの値によってスタイル変更 */}
               <span style={{textDecoration: task.check ? 'line-through' : 'none'}}>{task.content}</span>
               {/* tsだと () => method の形にしないとエラーが出る */}
               <input type='button' value="del" onClick={() => handleDelete(task.id)} /><br />
@@ -113,6 +135,7 @@ const Example = () => {
           )
         })
       }
+      </div>
       <Link to='/'>
         homeへ戻る
       </Link>
--- src/functions/createUnixTime.ts
+++ src/functions/createUnixTime.ts
@@ -1,5 +1,5 @@
 // 一意性のあるidを生成(現在の日時をunix時間にすれば被らない)
-const createUnixTime = () => {
+const createUnixTime = (): number => {
   const date = new Date()
   return date.getTime()
 }
